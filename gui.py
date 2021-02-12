aa5646f7701bd651edc499ae4b1ca8210a7cfcf3
Maurice Kraus <dev@mkraus.io>
2021-02-12 16:11:37 +0100

    included checkpoint parsing

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog, ttk
+import os
 
 
 class DLMBApp(tk.Tk):
@@ -12,8 +13,8 @@ class DLMBApp(tk.Tk):
 
         self.geometry("{}x{}".format(window_width, window_height))
 
-        position_right = int(window_width - window_width/2)
-        position_down = int(window_height - window_height/2)
+        position_right = int(window_width - window_width / 2)
+        position_down = int(window_height - window_height / 2)
 
         # Positions the window in the center of the page.
         self.geometry("+{}+{}".format(position_right, position_down))
@@ -23,8 +24,8 @@ class DLMBApp(tk.Tk):
         # it should be rendered
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-        #container.grid_rowconfigure(0, weight=1)
-        #container.grid_columnconfigure(0, weight=1)
+        # container.grid_rowconfigure(0, weight=1)
+        # container.grid_columnconfigure(0, weight=1)
         container.grid_columnconfigure(0, minsize=500)
         container.grid_rowconfigure(0, minsize=500)
 
@@ -53,27 +54,56 @@ class StartPage(tk.Frame):
         left_frame = tk.Frame(self)
         right_frame = tk.Frame(self)
 
-        left_frame.grid(row=0, column=0)
-        right_frame.grid(row=0, column=1)
+        left_frame.pack(ipadx=10, ipady=10, side="left", expand=True, fill="both")
+        right_frame.pack(ipadx=10, ipady=10, expand=True, fill="both")
 
+        self.checkpoint_list = ttk.Treeview(left_frame)
+        self.checkpoint_list.bind('<ButtonRelease-1>', func=self.select_checkpoint)
+        self.checkpoint_list.column("#0", width=270, minwidth=270, stretch=tk.NO)
 
+        self.checkpoint_list.heading("#0", text="Run", anchor=tk.W)
 
-        checkpoint_list = tk.Listbox(left_frame, width=50, height=20)
-        checkpoint_list.insert("end", "Bla")
-        checkpoint_list.pack(fill="x")
+        path = "./runs"
+
+        for root, directories, files in os.walk(path, topdown=False):
+            for name in directories:
+                folder_split = name.split("_")[0:2]
+                folder_name = folder_split[0] + "_" + folder_split[1]
+                folder_path = os.path.join(root, name)
+                folder_elements = os.listdir(folder_path)
+                if len(folder_elements) != 1:
+                    # want to skip if no elements are present
+                    folder = self.checkpoint_list.insert("", index=1, text=folder_name)
+
+                    for file in folder_elements:
+                        if "events" not in file:
+                            self.checkpoint_list.insert(folder, index="end", values=(folder_path + "/" + file),
+                                                        text=file)
+
+        self.checkpoint_list.pack()
 
         start_training_btn = tk.Button(right_frame, text="Start New Training",
                                        command=lambda: self.handle_training(new=True, checkpoint=None))
-        start_training_btn.grid(row=0, column=0)
 
         selected_checkpoint = None
         continue_training_btn = tk.Button(right_frame, text="Continue From Checkpoint",
                                           command=lambda: self.handle_training(new=False,
                                                                                checkpoint=selected_checkpoint))
-        continue_training_btn.grid(row=1, column=0)
 
-        load_model_btn = tk.Button(right_frame, text="Evaluate Model", command=lambda: self.handle_model_evaluation(None))
-        load_model_btn.grid(row=2, column=0, sticky=(tk.N, tk.S, tk.E, tk.W))
+        load_model_btn = tk.Button(right_frame, text="Evaluate Model",
+                                   command=lambda: self.handle_model_evaluation(None))
+        start_training_btn.pack()
+        continue_training_btn.pack()
+        load_model_btn.pack()
+
+    def select_checkpoint(self, a):
+        cur_item = self.checkpoint_list.focus()
+        values = self.checkpoint_list.item(cur_item)['values']
+        print(values)
+        if len(values) == 0:
+            self.checkpoint_list.selection_remove(cur_item)
+        else:
+            self.controller.selected_checkpoint = values[0]
 
     def handle_training(self, new, checkpoint):
         if new:
